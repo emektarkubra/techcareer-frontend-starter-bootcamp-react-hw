dffcb1ae9c58b6ba5cbfe534e557e0d9ddc9abf4
Kübra Emektar <emektarrkubra@gmail.com>
2023-10-18 13:16:17 +0300

    added hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import ArchitectTempateLayout from "./architectTemplate-hw/ArchitectTemplateLayout";
 import Products from "./objectArray-hw/Products";
 import BoxSize from "./state-active-hw/BoxSize";
+import StateApi from "./state-hw/StateApi";
 
 function App() {
   return (
     <>
       {/* <Products /> */}
       {/* <ArchitectTempateLayout /> */}
-      <BoxSize />
+      {/* <BoxSize /> */}
+      <StateApi />
     </>
   );
 }
